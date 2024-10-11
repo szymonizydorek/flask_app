d4703969284b742fa34a6f81352cb405a1758610
Szymon Izydorek <izydorek.szymon@gmail.com>
2024-10-11 16:42:19 +0200

    Adding templates

--- Flask_project.py
+++ Flask_project.py
@@ -1,11 +1,23 @@
 #!/opt/flask/bin/python
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "My first Flask Application"
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def index_login():
+    username = request.form['username']
+    password = request.form['password']
+    
+    if username == 'admin' and password == 'password':
+        return f'Welcome, {username}!'
+    else:
+        return f'{username} and your password are incorrect', 401
+ 
+    return f'Username: {username}, password:{password}'
 
 
 if __name__ == '__main__':
